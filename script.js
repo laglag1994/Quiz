2940345204d4c4d227658fab8eedcf6bfe7792a5
TajRask <roqayah1994@gmail.com>
2022-12-20 14:30:30 +0300

    Quiz

---
+++ script.js
@@ -0,0 +1,34 @@
+ const quizData=[
+
+    {
+        question:'who are the Queen and Queen of the castle? ',
+        a:'Trinity and Nader',
+        b:'Kiara and Kero',
+        c:'Goon and Malik',
+        d:'Eyad and 3aneed',
+        correct:'c'
+    },
+    {
+        question: 'how many fifis do we have?',
+        a:'2',
+        b:'3',
+        c:'4',
+        d:'5',
+        correct:'d'
+    },
+    {
+        question:"Who is my baby? ",
+        a: 'Trinity is your baby',
+        b: 'Malik is your baby',
+        c: 'Katana is your baby',
+        correct:'a'
+    },
+    {
+        question: 'will you marry me? ',
+        a: 'yes',
+        b: 'yes',
+
+        correct: 'a'& 'b'
+    }
+
+ ]
